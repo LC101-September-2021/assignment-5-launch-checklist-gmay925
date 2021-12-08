f772f120172552ce1c9c0646784e0198944a2a38
Gia <gmay925@yahoo.com>
2021-12-08 06:09:38 -0500

    finished assignment

--- script.js
+++ script.js
@@ -1,16 +1,33 @@
 // Write your JavaScript code here!
-
 window.addEventListener("load", function() {
 
+    let list = document.getElementById('faultyItems');
+
+    let form = document.querySelector("form");
+    form.setAttribute('onsubmit', 'return false');
+    form.addEventListener("submit", function(event){
+        let pilotName = document.querySelector("input[name=pilotName]").value;
+        let copilotName = document.querySelector("input[name=copilotName]").value;
+        let fuelLevel = document.querySelector("input[name=fuelLevel]").value;
+        let cargoMass = document.querySelector("input[name=cargoMass]").value;
+
+        formSubmission(document, list, pilotName, copilotName, fuelLevel, cargoMass);
+       
+    })
+
+   
    let listedPlanets;
    // Set listedPlanetsResponse equal to the value returned by calling myFetch()
-   let listedPlanetsResponse;
+   let listedPlanetsResponse = myFetch();
    listedPlanetsResponse.then(function (result) {
        listedPlanets = result;
        console.log(listedPlanets);
+    
    }).then(function () {
        console.log(listedPlanets);
        // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
+       let planet = pickPlanet(listedPlanets);
+       addDestinationInfo(document, planet.name, planet.diameter, planet.star, planet.distance, planet.moons, planet.image);
    })
-   
+ 
 });
